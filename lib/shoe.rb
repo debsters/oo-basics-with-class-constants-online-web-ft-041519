29936b6d4e0d1e97c1f464ece35753462a2a9d4d
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-04-27 20:05:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,11 +8,14 @@ class Shoe
     @brand = brand
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRAND << brand
+  end
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-BRAND << brand
 
 end
